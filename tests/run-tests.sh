957845514a7bcaa1aee7e7862f76308da40fb432
Xunrong Li <xunrongli@us.ibm.com>
2018-03-14 17:10:16 -0500

    add missing spec file

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,9 +2,9 @@
 GRUNTFILE="./tests/Gruntfile.js"
 if [ -f $GRUNTFILE ]; then
 export PATH=/opt/IBM/node-v4.2/bin:$PATH
-# npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
+npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
 npm install
-npm install grunt-idra3
+npm install -g grunt-idra3
 
 set +e
 grunt dev-test-cov --no-color --gruntfile $GRUNTFILE --base .
