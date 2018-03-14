9bff4358a80bc553df28848a48cf08ce7ada697c
Xunrong Li <xunrongli@us.ibm.com>
2018-03-14 16:35:06 -0500

    update to use the real tests

--- tests/Gruntfile.js
+++ tests/Gruntfile.js
@@ -8,6 +8,76 @@ var gruntConfig = {
         env: {
             // dynamically filled
         },
+        mochaTest: {
+            'server-side': {
+                options: {
+                    reporter: 'XUnit',
+                    clearRequireCache: true,
+                    colors: true,
+                    quite: true,
+                    captureFile: 'tests/server/mochatest.xml',
+                    gruntLogHeader: false
+                },
+                src: ['tests/server/*.spec.js']
+            },
+            'server-side-spec': {
+                options: {
+                    reporter: 'spec',
+                    clearRequireCache: true,
+                    colors: true,
+                    quite: true
+                },
+                src: ['tests/server/*.spec.js']
+            }
+        },
+        clean: {
+            options: {
+                force: true,
+                expand: true
+            },
+            coverage: ['tests/server/coverage', 'tests/server/mochatest.json', 'tests/fvt/mochafvttest.json']
+        },
+        storeCoverage: {
+            options: {
+                dir: 'tests/server/coverage/reports'
+            }
+        },
+
+        makeReport: {
+            src: 'tests/server/coverage/reports/*.json',
+            options: {
+                type: 'html',
+                type: 'json-summary',
+                dir: 'tests/server/coverage/reports',
+                file: 'coverage-summary.json'
+                //print: 'detail'
+            }
+        },
+
+        'makeReport-lcov': {
+            src: 'tests/server/coverage/reports/*.json',
+            options: {
+                type: 'lcov',
+                dir: 'tests/server/coverage/reports'
+            }
+        },
+        copy: {
+            resourcesForInstrumented: {
+                nonull: true,
+                files: [{
+                    expand: true,
+                    dest: 'tests/server/coverage/instrumented',
+                    src: ['routes/db.js']
+                }]
+            }
+        },
+        instrument: {
+            files: ['routes/orders.js'],
+            options: {
+                lazy: false,
+                basePath: 'tests/server/coverage/instrumented/'
+            }
+        },
         simplemocha: {
             sauce: {
                 options: {
@@ -68,14 +138,22 @@ module.exports = function(grunt) {
     grunt.initConfig(gruntConfig);
 
     // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-istanbul');
     grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.renameTask('makeReport', 'makeReport-lcov');
+    grunt.loadNpmTasks('grunt-istanbul');
 
     grunt.registerTask('test_real', ['env:chrome', 'simplemocha:sauce:' + _(desireds).keys().first()]);
     grunt.registerTask('test_fake', ['env:chrome', 'simplemocha:sauce_node:' + _(desireds).keys().first()]);
+    grunt.registerTask('dev-test-cov', ['env', 'clean:coverage', 'copy:resourcesForInstrumented', 'instrument', 'mochaTest:server-side', 'storeCoverage', 'makeReport-lcov', 'makeReport']);
 
     // _.forIn(desireds,function(desired, key) {
     //         grunt.registerTask('test:sauce:' + key, ['env:' + key, 'simplemocha:sauce']);
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-
+GRUNTFILE="./tests/Gruntfile.js"
+if [ -f $GRUNTFILE ]; then
 export PATH=/opt/IBM/node-v4.2/bin:$PATH
-npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
+# npm install -g npm@3.7.2 ### work around default npm 2.1.1 instability
 npm install
-npm install -g grunt-idra3
+npm install grunt-idra3
 
-idra --publishtestresult --filelocation=./tests/mochatest.xml --type=unittest
-idra --publishtestresult --filelocation=./tests/coverage-summary.json --type=code
+set +e
+grunt dev-test-cov --no-color --gruntfile $GRUNTFILE --base .
+grunt_result=$?
+set -e
+
+idra --publishtestresult --filelocation=./tests/server/mochatest.xml --type=unittest
+idra --publishtestresult --filelocation=./tests/server/coverage/reports/coverage-summary.json --type=code
+
+if [ $grunt_result -ne 0 ]; then
+    exit $grunt_result
+fi
+else
+echo "$GRUNTFILE not found."
+fi
