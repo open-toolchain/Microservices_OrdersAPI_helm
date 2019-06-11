d38c5a0c8f6a3a9fd9470bd5b84a930c411e31e4
Philippe Mulet <hmagph@users.noreply.github.com>
2019-06-11 14:36:01 +0200

    Update run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 GRUNTFILE="./tests/Gruntfile.js"
 if [ -f $GRUNTFILE ]; then
-export PATH=/opt/IBM/node-v6.7.0/bin:$PATH
-npm install
-npm install -g grunt-idra3
+  export PATH=/opt/IBM/node-v6.7.0/bin:$PATH
+  npm install
+  npm install -g grunt-idra3
 
-set +e
-grunt dev-test-cov --no-color --gruntfile $GRUNTFILE --base .
-grunt_result=$?
-set -e
+  set +e
+  grunt dev-test-cov --no-color --gruntfile $GRUNTFILE --base .
+  grunt_result=$?
+  set -e
 
-idra --publishtestresult --filelocation=./tests/server/mochatest.xml --type=unittest
-idra --publishtestresult --filelocation=./tests/server/coverage/reports/coverage-summary.json --type=code
+  idra --publishtestresult --filelocation=./tests/server/mochatest.xml --type=unittest
+  idra --publishtestresult --filelocation=./tests/server/coverage/reports/coverage-summary.json --type=code
 
-if [ $grunt_result -ne 0 ]; then
+  if [ $grunt_result -ne 0 ]; then
     exit $grunt_result
-fi
+  fi
 else
-echo "$GRUNTFILE not found."
+  echo "$GRUNTFILE not found."
 fi
