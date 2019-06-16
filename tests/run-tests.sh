7da501ed2117647aaf304a4de5592212801cca30
Philippe Mulet <hmagph@users.noreply.github.com>
2019-06-16 21:31:35 +0200

    convert to doi

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,15 +3,16 @@ GRUNTFILE="./tests/Gruntfile.js"
 if [ -f $GRUNTFILE ]; then
   export PATH=/opt/IBM/node-v6.7.0/bin:$PATH
   npm install
-  npm install -g grunt-idra3
+  npm install -g grunt-cli
 
   set +e
   grunt dev-test-cov --no-color --gruntfile $GRUNTFILE --base .
   grunt_result=$?
   set -e
 
-  idra --publishtestresult --filelocation=./tests/server/mochatest.xml --type=unittest
-  idra --publishtestresult --filelocation=./tests/server/coverage/reports/coverage-summary.json --type=code
+  ibmcloud login --apikey $IBM_CLOUD_API_KEY --no-region
+  ibmcloud doi publishtestrecord --logicalappname="$APP_NAME" --buildnumber=$BUILD_NUMBER --filelocation=./tests/server/mochatest.xml --type=unittest
+  ibmcloud doi publishtestrecord --logicalappname="$APP_NAME" --buildnumber=$BUILD_NUMBER --filelocation=./tests/server/coverage/reports/coverage-summary.json --type=code
 
   if [ $grunt_result -ne 0 ]; then
     exit $grunt_result
