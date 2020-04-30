4e2fe87b97b946a8fc416060f302d2f11049a54e
Philippe Mulet <hmagph@users.noreply.github.com>
2020-04-30 14:39:07 +0200

    Decouple DOI from test run script
    See https://github.ibm.com/org-ids/roadmap/issues/12524

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 GRUNTFILE="./tests/Gruntfile.js"
 if [ -f $GRUNTFILE ]; then
+  export PATH=/opt/IBM/node-v6.7.0/bin:$PATH
   npm install
   npm install -g grunt-cli
 
@@ -8,14 +9,10 @@ if [ -f $GRUNTFILE ]; then
   grunt dev-test-cov --no-color --gruntfile $GRUNTFILE --base .
   grunt_result=$?
   set -e
-
-  ibmcloud login --apikey $IBM_CLOUD_API_KEY --no-region
-  ibmcloud doi publishtestrecord --logicalappname="$APP_NAME" --buildnumber=$BUILD_NUMBER --filelocation=./tests/server/mochatest.xml --type=unittest
-  ibmcloud doi publishtestrecord --logicalappname="$APP_NAME" --buildnumber=$BUILD_NUMBER --filelocation=./tests/server/coverage/reports/coverage-summary.json --type=code
-
-  if [ $grunt_result -ne 0 ]; then
-    exit $grunt_result
-  fi
+  
+  FILE_LOCATIONS="./tests/server/mochatest.xml;./tests/server/coverage/reports/coverage-summary.json"
+  TEST_TYPES="unittest;code"
+  TEST_STATUS=$grunt_result
 else
   echo "$GRUNTFILE not found."
 fi
